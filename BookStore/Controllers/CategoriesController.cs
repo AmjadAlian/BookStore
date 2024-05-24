f0c82834d5adb7e7277a59c50c3b386aaa2fee70
amjad alian <amjad@amjadad.onmicrosoft.com>
2024-05-24 22:52:04 +0300

    #show Categories

---
+++ BookStore/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using BookStore.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        ApplicationDbContext context;
+
+       public CategoriesController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var Categories = context.categories.ToList();
+            return View(Categories);
+        }
+    }
+}
---
+++ BookStore/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [MaxLength(30)]
+        public string Name { get; set; } = null!;
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public DateTime LastUpdatedDate { get; set; } = DateTime.Now;
+    }
+}
