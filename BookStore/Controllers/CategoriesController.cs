6c752c636517ce4609467ee48bab50e0f8e1761c
amjad alian <amjad@amjadad.onmicrosoft.com>
2024-05-28 23:11:49 +0300

    show Category data and add CRUD for categories

--- BookStore/Controllers/CategoriesController.cs
+++ BookStore/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
 ﻿using BookStore.Data;
+using BookStore.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore.Controllers
@@ -7,7 +8,7 @@ namespace BookStore.Controllers
     {
         ApplicationDbContext context;
 
-       public CategoriesController(ApplicationDbContext context)
+        public CategoriesController(ApplicationDbContext context)
         {
             this.context = context;
         }
@@ -16,5 +17,59 @@ namespace BookStore.Controllers
             var Categories = context.categories.ToList();
             return View(Categories);
         }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category CategoryVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", CategoryVM);
+            }
+            var category = new Category()
+            {
+                Name = CategoryVM.Name,
+            };
+            context.categories.Add(category);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            
+
+            return View("Create");
+
+        }
+        [HttpPost]
+        public IActionResult Edit(Category CategoryVM) {
+
+
+            var category = context.categories.Find(CategoryVM.Id);
+            category.Name = CategoryVM.Name;
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var category = context.categories.Find(id);
+
+            context.Remove(category);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        
     }
+
+   
 }
+   
---
+++ BookStore/ViewModel/CategoryVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.ViewModel
+{
+    public class CategoryVM
+    {
+        public int Id { get; set; }
+
+        [MaxLength(30)]
+        public string Name { get; set; } = null!;
+    }
+}
